8f5df277bd6540098cf3dbd0177e8e93e76034ff
khanh1908 <81968671+khanh1908@users.noreply.github.com>
2021-12-13 20:57:12 +0700

    Update snake.py

--- Snake/snake.py
+++ Snake/snake.py
@@ -5,6 +5,7 @@ import random
 from pygame import mixer
 mixer.init()
 pygame.init()
+# veloc=100
 # Color
 red = pygame.Color(255, 0, 0)
 blue = pygame.Color(65, 105, 255)
@@ -17,7 +18,7 @@ y = 600
 # Set display
 Screen = pygame.display.set_mode((x, y))
 # Set Name
-pygame.display.set_caption('Snake')
+pygame.display.set_caption('Snake Game')
 # kích thước chiều cao và chiều rộng
 m = 20
 n = 10
@@ -51,6 +52,7 @@ body = pygame.transform.scale(body, (m, m))
 food = pygame.image.load('apple.png')
 food = pygame.transform.scale(food, (m, m))
 
+
 # Load music
 # sound=pygame.mixer.Sound('no6.wav')
 mixer.music.load('no6.wav')
@@ -65,8 +67,8 @@ def button(msg, x, y, l, w, oc, bc, action=None):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
 
-    if x + l > mouse[0] > x and y + w > mouse[1] > y:
-        pygame.mixer.Sound.play(sound1)
+    if x < mouse[0] < x + l and y < mouse[1] < y + w:
+        # pygame.mixer.Sound.play(sound1)
         pygame.draw.rect(Screen, bc, (x, y, l, w))
         if click[0] == 1 and action != None:
             action()
@@ -74,7 +76,7 @@ def button(msg, x, y, l, w, oc, bc, action=None):
         # pass
         pygame.draw.rect(Screen, oc, (x, y, l, w))
     text = pygame.font.SysFont("bahnschrift", 30).render(msg, True, black)
-    text_rect = text.get_rect(center= ((x + (l / 2)), (y+(w/2))))
+    text_rect = text.get_rect(center=((x + (l / 2)), (y+(w/2))))
     Screen.blit(text, text_rect)
 #  Hàm bỏ pause game
 def unpause():
@@ -119,8 +121,9 @@ def game_intro():
         pygame.display.update()
 # hàm gameover
 def game_over(score):
-    # pygame.mixer.Sound.play(sound)
-    # pygame.mixer.music.stop()
+   # pygame.mixer.Sound.play(sound1)
+   #  pygame.mixer.music.stop()
+
     text = pygame.font.SysFont('consolas', 40).render('Game over!', True, red)
     Screen.blit(text, [300, 300])
     font = pygame.font.SysFont('consolas', 28).render('Score:' + str(score), True, white)
@@ -136,18 +139,32 @@ def game_over(score):
         button("Menu", 380, 450, 100, 50, gray, red, game_intro)
         button("Quit", 550, 450, 100, 50, gray, red, quitgame)
         pygame.display.update()
-# vòng lặp chính
-# pygame.mixer.Sound.play(sound)
+def draw():
+    # Nền khung rắn di chuyển
+    Screen.blit(Background, (0, 56))
+    # nền khung điểm
+    Screen.blit(Background1, (0, 0))
+    # rào chắn
+    # biên trên
+    Screen.blit(wall, (0, 56))
+    # biên dưới
+    Screen.blit(wall, (0, 590))
+    # biên dọc trái
+    Screen.blit(wall1, (0, 56))
+    # biên dọc phải
+    Screen.blit(wall1, (790, 56))
+    # draw food score
+    Screen.blit(food, (128, 15))
 def game():
     # game_over()
-    # game_over1()
     # pygame.mixer.Sound.play(sound)
     global pause
     snakepos = [200, 100]
     snakebody = [[200, 100], [180, 80]]
+    veloc= 100
 
-    foodx = round(random.randrange(30, x - 30) / 10)
-    foody = round(random.randrange(80, y - 30) / 10)
+    foodx = round(random.randrange(40, x - 40) / 10)
+    foody = round(random.randrange(80, y - 40) / 10)
 
     if foodx % 2 != 0:
         foodx += 1
@@ -161,9 +178,9 @@ def game():
     # button("Quit", 550, 450, 100, 50, gray, red, quitgame)
     play = True
     while play:
-        # pygame.mixer.Sound.play(sound)
+        # pygame.mixer.Sound.play(sound1)
         # tốc độ chơi
-        pygame.time.delay(100)
+        pygame.time.delay(int(veloc))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -205,6 +222,8 @@ def game():
         snakebody.insert(0, list(snakepos))
         if snakepos[0] == foodpos[0] and snakepos[1] == foodpos[1]:
             score += 1
+            veloc=veloc*0.98
+            print(veloc)
             pygame.mixer.Sound.play(sound1)
             # pygame.display.update()
             foodflat = False
@@ -212,8 +231,8 @@ def game():
             snakebody.pop()
         # food
         if foodflat == False:
-            foodx = round(random.randrange(30, x - 30) / 10)
-            foody = round(random.randrange(80, y - 30) / 10)
+            foodx = round(random.randrange(30, x - 40) / 10)
+            foody = round(random.randrange(80, y - 40) / 10)
             if foodx % 2 != 0:
                 foodx += 1
             if foody % 2 != 0:
@@ -222,20 +241,7 @@ def game():
             foodflat = True
         #  cập nhật lên cửa sổ
             print(foodpos)
-        # Nền khung rắn di chuyển
-        Screen.blit(Background, (0, 56))
-        # nền khung điểm
-        Screen.blit(Background1, (0, 0))
-        # rào chắn
-            # biên trên
-        Screen.blit(wall, (0, 56))
-            # biên dưới
-        Screen.blit(wall, (0, 590))
-            # biên dọc trái
-        Screen.blit(wall1, (0, 56))
-            # biên dọc phải
-        Screen.blit(wall1, (790, 56))
-
+        draw()
         for pos in snakebody:
             Screen.blit(body, pygame.Rect(pos[0], pos[1], m, m))
         Screen.blit(head, pygame.Rect(snakebody[0][0], snakebody[0][1], m, m))
@@ -243,19 +249,18 @@ def game():
 
         # Các trường hợp game_over
         # 1. Đâm vào tường
-        if snakepos[0] > 780 or snakepos[0] < 15:
+        if snakepos[0] > 770 or snakepos[0] < 15:
             game_over(score)
         if snakepos[1] > 575 or snakepos[1] < 60:
             game_over(score)
         # 2. Snake tự cắn vào cơ thể
-        for bodysnake in snakebody[1:]:
+        for bodysnake in snakebody[5:]:
             if snakepos[0] == bodysnake[0] and snakepos[1] == bodysnake[1]:
                 game_over(score)
         # In ra điểm trong khi game chạy
         font = pygame.font.SysFont('consolas', 28)
         Score = font.render("Score:" + str(score), True, red)
         Screen.blit(Score, (5, 15))
-        Screen.blit(food, (128, 15))
         pygame.display.flip()
         # print(snakebody)
         # print(score)
